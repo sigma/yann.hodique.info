c98a82f81c9407753623475f73400da1f6b5d11d
Yann Hodique <hodiquey@vmware.com>
2017-07-23 02:07:19 -0700

    handle raw bib files

---
+++ static/bib/auto/biblio.el
@@ -0,0 +1,23 @@
+(TeX-add-style-hook
+ "biblio"
+ (lambda ()
+   (LaTeX-add-bibitems
+    "ecoop04"
+    "rose98lightweight"
+    "blanchet99:_escap_objec_orien_languag"
+    "gay00fast"
+    "whaley99compositional"
+    "358283"
+    "grimaud99facade"
+    "design-patterns"
+    "egh94pldi"
+    "wl95pldi"
+    "park92escape"
+    "CousotCousot77-1"
+    "GhindiciMaster"
+    "902450"
+    "GallandPhd"
+    "whaley-efficient"
+    "emami94contextsensitive"))
+ :bibtex)
+
